9ffd20a2fa91fee3344c240c2856567be535634d
scidejump <scidejump@outlook.com>
2025-10-06 21:40:07 +0800

    Update acids.js
    once again changed pub unlock for tseting purposes

--- acids.js
+++ acids.js
@@ -96,7 +96,7 @@ var init = () => {
 
     /////////////////////
     // Permanent Upgrades
-    theory.createPublicationUpgrade(0, currency, 1e3);
+    theory.createPublicationUpgrade(0, currency, 1e1);
     theory.createBuyAllUpgrade(1, currency, 1e8);
     theory.createAutoBuyerUpgrade(2, currency, 1e6);
 
